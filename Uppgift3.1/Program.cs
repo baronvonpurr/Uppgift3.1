eae6eb012d4094be005db91cd01de333ee1904d2
baronvonpurr <robinhood.svensson@hotmail.com>
2023-10-04 10:56:25 +0200

    Add project files.

---
+++ Uppgift3.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Uppgift3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder > 19)
+            {
+                Console.WriteLine("Du är för gammal!");
+                Console.ReadKey();
+            }
+            else if (ålder < 16)
+            {
+                Console.WriteLine("Du är för ung!");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Du får vara med!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
